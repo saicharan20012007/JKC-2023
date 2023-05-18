af9669b0490bd42c4c4b701e8ad9dd346b08d9e0
Sai Charan <92617073+saicharan20012007@users.noreply.github.com>
2023-05-18 06:38:40 +0530

    Update index.js

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -1,45 +1,99 @@
 import React, { useState } from 'react';
 
-import { NavContainer, SubNavContainer,ProfileDetails, UserName, UserImage,LogoutText,AttendanceIcon,LogoutIcon, SubNavItem ,ReportIcon,HomeIcon, NavItem, LinkItem,UpdateIcon} from './DashBoardStyledComponents';
+import { NavContainer, Designation,SubNavContainer,ProfileDetails, UserName,RightIcon, DownIcon, UserImage,LogoutText,AttendanceIcon,LogoutIcon, SubNavItem ,ReportIcon,HomeIcon, NavItem, LinkItem,UpdateIcon} from './DashBoardStyledComponents';
 
 
 
 const Dashboard = () => {
   const [activeNavItem, setActiveNavItem] = useState('home');
   const [activeSubNavItem, setActiveSubNavItem] = useState(null);
+  const [isHomeActive, setHomeActive] = useState(true);
+  const [isMyUpdatesActive, setMyUpdatesActive] = useState(false);
+  const [isMyReportsActive, setReportsActive] = useState(false);
+  const [isMyAttendanceActive, setAttendanceActive] = useState(false);
 
-  const handleNavItemClick = (navItem) => {
-    setActiveNavItem(navItem);
-    setActiveSubNavItem(null);
-  };
+
+
+
+
+  
+
+  // const handleNavItemClick = (navItem) => {
+  //   setActiveNavItem(navItem);
+  //   setActiveSubNavItem(null);
+  // };
 
   const handleSubNavItemClick = (subNavItem) => {
     setActiveSubNavItem(subNavItem);
   };
+
+  const clickedHome = (navItem) => {
+    setHomeActive(!isHomeActive);
+    setMyUpdatesActive(false)
+    setReportsActive(false)
+    setAttendanceActive(false)
+    setActiveNavItem('home');
+    setActiveSubNavItem(null);
+  }
+  const clickedMyUpdates = () => {
+    setHomeActive(false)
+    setMyUpdatesActive(!isMyUpdatesActive)
+    setReportsActive(false)
+    setActiveNavItem('my-updates');
+
+
+    setAttendanceActive(false)
+    setActiveSubNavItem(null);
+
+  }
+  const clickedReports= () => {
+    setHomeActive(false)
+    setMyUpdatesActive(false)
+    setReportsActive(!isMyReportsActive)
+    setAttendanceActive(false)
+    setActiveNavItem('reports');
+    setActiveSubNavItem(null);
+
+
+  }
+  const clickedAttendance= () => {
+    setHomeActive(false)
+    setMyUpdatesActive(false)
+    setReportsActive(false)
+    setAttendanceActive(!isMyAttendanceActive)
+    setActiveNavItem('attendance');
+    setActiveSubNavItem(null);
+
+
+  }
+
 
   return (
     <>
       <NavContainer>
       <ProfileDetails>
                     <UserImage src="https://www.pngall.com/wp-content/uploads/5/User-Profile-PNG.png" alt="User Profile"/>
-                    <UserName>Y Venkateswara Rao</UserName>
-                    <LogoutText>Logout <LogoutIcon/></LogoutText>
+                    <div><UserName>Y Venkateswara Rao</UserName>
+                    <Designation>HOD</Designation>
+         <LogoutText>Logout <LogoutIcon/></LogoutText></div>
                 </ProfileDetails>
         <LinkItem to = "/">
          <NavItem
-          isActive={activeNavItem === 'home'}
-          onClick={() => handleNavItemClick('home')}
+          isActive={activeNavItem==='home'}
+          // onClick={() => handleNavItemClick('home')}
+          onClick={clickedHome}
         >
         <HomeIcon/>   Home
         </NavItem></LinkItem>
         <LinkItem to="/my-updates">
         <NavItem
           isActive={activeNavItem === 'my-updates'}
-          onClick={() => handleNavItemClick('my-updates')}
+          // onClick={() => handleNavItemClick('my-updates')}
+          onClick={clickedMyUpdates}
         >
-       <UpdateIcon/> My Updates
+       <UpdateIcon/> My Updates {isMyUpdatesActive ? <DownIcon/> : <RightIcon/>}
         </NavItem></LinkItem>
-        {activeNavItem === 'my-updates' && (
+        {isMyUpdatesActive && (
           <SubNavContainer>
             <LinkItem to="/update-password">
             <SubNavItem
@@ -90,12 +144,12 @@ const Dashboard = () => {
         <LinkItem to="reports">
         <NavItem
           isActive={activeNavItem === 'reports'}
-          onClick={() => handleNavItemClick('reports')}
+          onClick={clickedReports}
         >
-        <ReportIcon/>  Reports
+        <ReportIcon/>  Reports {isMyReportsActive ? <DownIcon/> : <RightIcon/>}
         </NavItem>
         </LinkItem>
-        {activeNavItem === 'reports' && (
+        {isMyReportsActive && (
           <SubNavContainer>
             <LinkItem to="/student-details">            <SubNavItem
               isActive={activeSubNavItem === 'student-details'}
@@ -124,12 +178,12 @@ const Dashboard = () => {
                         <LinkItem to="/attendance">
               <NavItem
               isActive={activeNavItem === 'attendance'}
-              onClick={() => handleNavItemClick('attendance')}
+              onClick={clickedAttendance}
               >
-             <AttendanceIcon/> Attendance
+             <AttendanceIcon/> Attendance {isMyAttendanceActive ? <DownIcon/> : <RightIcon/>}
               </NavItem>
               </LinkItem>
-              {activeNavItem === 'attendance' && (
+              {isMyAttendanceActive && (
               <SubNavContainer>
                           <LinkItem to="/enter-attendance">
               <SubNavItem
