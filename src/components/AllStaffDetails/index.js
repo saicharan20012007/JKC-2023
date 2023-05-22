cfdfe6ec4368e1d8e33936d8f859d7ed51e449bd
Sai Charan <saicharan20012007@gmail.com>
2023-05-22 11:22:52 +0530

    sample test

--- src/components/AllStaffDetails/index.js
+++ src/components/AllStaffDetails/index.js
@@ -3,7 +3,7 @@ import {BgContainer,MaintainImage, PageText} from './ASD'
 const AllStaffDetails = () => (
     <BgContainer>
         <MaintainImage src="https://res.cloudinary.com/dlovqnrza/image/upload/v1684135277/maintain_moazzq.webp" alt="Maintaining"/>
-            <PageText>All Staff Details Page</PageText>
+            <PageText>All Staff Detail Page</PageText>
     </BgContainer>
 )
 
